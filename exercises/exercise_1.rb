f08648761d22dee9a2fb5eb37be98e753addc02c
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-14 22:44:47 +0000

    rename the instances

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -10,8 +10,8 @@ puts "----------"
 # 
 # 2 Output (puts) the number of the stores using ActiveRecord's count method, to ensure that there are three stores in the database.
 
-burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+@store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+@store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+@store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
